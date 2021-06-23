c5ab329386144ab8857fe8828422361a91028190
Samira Alizadehzoj <samira.alizadehzoj@gmail.com>
2021-06-23 18:00:46 -0400

    add each context that is about corona and set of it's Q/A to the dataset file

--- dataset/covid-qa.py
+++ dataset/covid-qa.py
@@ -6,17 +6,19 @@ data = json_data['data']
 
 f_out = open('COVID-QA.txt', 'a')
 
-for context in range(0, len(data)):
-    for parag in range(0, len(data[context]['paragraphs'])):
-        for qas in range(0, len(data[context]['paragraphs'][parag]['qas'])):
-            q = data[context]['paragraphs'][parag]['qas'][qas]['question']
-            for ans in range(0, len(data[context]['paragraphs'][parag]['qas'][qas]['answers'])):
-                a = data[context]['paragraphs'][parag]['qas'][qas]['answers'][ans]['text']
-            if q.find('corona') > 0:
+for section in range(0, len(data)):
+    for parag in range(0, len(data[section]['paragraphs'])):
+        context = data[section]['paragraphs'][parag]['context']
+        if context.find('corona') > 0:
+            f_out.writelines(
+                "\n\n######################### section: " + str(section) + " ################################\n")
+            f_out.writelines("context: " + context + "\n")
+            for qas in range(0, len(data[section]['paragraphs'][parag]['qas'])):
+                q = data[section]['paragraphs'][parag]['qas'][qas]['question']
+                for ans in range(0, len(data[section]['paragraphs'][parag]['qas'][qas]['answers'])):
+                    a = data[section]['paragraphs'][parag]['qas'][qas]['answers'][ans]['text']
                 f_out.writelines("question: " + q + "\n")
                 f_out.writelines("answer: " + a + "\n\n")
-
-
 
 f_in.close()
 f_out.close()
