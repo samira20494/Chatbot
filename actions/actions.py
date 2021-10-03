61595c4ebfb253d31023367254fa0effa1d32b49
Samira Alizadehzoj <samira.alizadehzoj@gmail.com>
2021-10-03 19:22:07 -0400

    update requirements

--- actions/actions.py
+++ actions/actions.py
@@ -11,11 +11,12 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-# import tensorflow as tf
-# from transformers import TFAutoModelForQuestionAnswering
-# from transformers import AutoTokenizer
-# import src.config as cf
-# from src.preprocessing import load_context_for_inference
+import tensorflow as tf
+from transformers import TFAutoModelForQuestionAnswering
+from transformers import AutoTokenizer
+import json
+from pathlib import Path
+
 
 
 class ActionCovid(Action):
@@ -28,25 +29,40 @@ class ActionCovid(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         question = tracker.latest_message['text']
-        # text = load_context_for_inference("src/data/COVID-QA.json")
-        #
-        # tokenizer = AutoTokenizer.from_pretrained(cf.setting["model_checkpoint"])
-        # model = TFAutoModelForQuestionAnswering.from_pretrained(cf.setting["model_checkpoint"])
-        #
-        # inputs = tokenizer(question, text, add_special_tokens=True, return_tensors="tf", truncation=True)
-        # input_ids = inputs["input_ids"].numpy()[0]
-        #
-        # output = model(inputs)
-        # answer_start = tf.argmax(
-        #     output.start_logits, axis=1
-        # ).numpy()[0]  # Get the most likely beginning of answer with the argmax of the score
-        # answer_end = (
-        #         tf.argmax(output.end_logits, axis=1) + 1
-        # ).numpy()[0]  # Get the most likely end of answer with the argmax of the score
-        # answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
-        # start = answer.index("SEP") + 4
-        # answer = answer[start:-5]
-        # dispatcher.utter_message(text=f"{'this is the answer!'}")
-        dispatcher.utter_message(text='ACTION SERVICE RESPONSE PLACEHOLDER')
+        text = load_context_for_inference("COVID-QA.json")
+
+        tokenizer = AutoTokenizer.from_pretrained("mrm8488/bert-small-finetuned-squadv2")
+        model = TFAutoModelForQuestionAnswering.from_pretrained("mrm8488/bert-small-finetuned-squadv2")
+
+        inputs = tokenizer(question, text, add_special_tokens=True, return_tensors="tf", truncation=True)
+        input_ids = inputs["input_ids"].numpy()[0]
+
+        output = model(inputs)
+        answer_start = tf.argmax(
+            output.start_logits, axis=1
+        ).numpy()[0]  # Get the most likely beginning of answer with the argmax of the score
+        answer_end = (
+                tf.argmax(output.end_logits, axis=1) + 1
+        ).numpy()[0]  # Get the most likely end of answer with the argmax of the score
+        answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
+        start = answer.index("SEP") + 4
+        answer = answer[start:-5]
+        dispatcher.utter_message(text=f"Answer is: {answer}")
+        # dispatcher.utter_message(text='Response from custom action!')
 
         return []
+
+
+def load_context_for_inference(path):
+    path = Path(path)
+    with open(path, 'rb') as f:
+        json_data = json.load(f)
+        data = json_data["data"]
+
+    context = ""
+
+    for section in range(0, len(data)):
+        for parag in range(0, len(data[section]["paragraphs"])):
+            context += data[section]["paragraphs"][parag]["context"]
+
+    return context
